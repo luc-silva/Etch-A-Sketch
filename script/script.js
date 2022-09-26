a46f4d56346473fe687c8de9df767d7db0a52a76
Lucas Santos <lucas.silva1203@outlook.com>
2022-09-26 18:03:41 -0300

    Add new functions and event listeners
    Add eraser, random and paint functions, event listeners for both
    checkboxes and displays

--- script/script.js
+++ script/script.js
@@ -1,13 +1,66 @@
-let enableGrid = document.querySelector("grid-checkbox")
+let enableClick = document.querySelector("#click-checkbox")
+let enableGrid = document.querySelector("#grid-checkbox")
+let colorSelector = document.querySelector("#color-selector")
+let randomColor = document.querySelector("#random")
 let mainScreen = document.querySelector("#screen")
+let eraser = document.querySelector("#eraser")
+let lastColors = []
+let color = "#000000"
 
-// enableGrid ? pixel.style.outline == "rgb(212, 212, 212) solid 1px" : pixel.style.outline == "none"
+randomColor.addEventListener("click", randomizer)
+function randomizer() {
+    let colors = ['#327abd', '#324abd', '#7132bd', '#bd3298', '#bd3232', '#bd8a32', '#adbd32', '#63bd32', '#32bd4a', '#32bd8f']
+    color = colors[Math.round(Math.random() * 10)]
 
-function size(){
-    for(i = 0; i < 16**2; i++){
-        let pixel = document.createElement("div")
-        pixel.classList.add("pixel")
-        mainScreen.appendChild(pixel)
-    }
+    return color
 }
-size()
+
+eraser.addEventListener("click", () => {
+    color = "#FFFFFF"
+})
+
+for(i = 0; i < 16**2; i++){
+    let pixel = document.createElement("div")
+    pixel.classList.add("pixel")
+    mainScreen.appendChild(pixel)
+}
+let pixels = document.querySelectorAll(".pixel")
+
+colorSelector.addEventListener("change", () => {
+    if(lastColors.length >= 5){
+        lastColors.pop()
+    }
+    lastColors.unshift(color)
+    color = colorSelector.value
+})
+
+enableClick.addEventListener("change", checkEnableCLick)
+function checkEnableCLick(){
+    (enableClick.checked)
+    ? pixels.forEach((pixel) => {
+        pixel.removeEventListener("mouseover", paint)
+        pixel.addEventListener("click", paint)
+    })
+    : pixels.forEach((pixel) => {
+        pixel.removeEventListener("click", paint)
+        pixel.addEventListener("mouseover", paint)
+    });
+}
+
+enableGrid.addEventListener("change", checkEnableGrid)
+function checkEnableGrid() {
+    (enableGrid.checked)
+    ? pixels.forEach((pixel) => {
+        pixel.style.outline = "rgb(212, 212, 212) solid 1px"
+    })
+    : pixels.forEach((pixel) => {
+        pixel.style.outline = "none"
+    })
+}
+
+
+function paint(){
+    this.style.backgroundColor = color
+    console.log("oi")
+}
+checkEnableCLick()
